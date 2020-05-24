04a629fb909a5b263867d29a7ec2147648b4b603
PB <b.piotr@gmail.com>
2020-05-24 22:41:52 +0200

    Added command for caching users data

---
+++ 2/application/app/Console/Commands/CacheUsers.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\JsonPlaceHolderClient\JsonPlaceHolderClient;
+use App\Services\User\UserServiceInterface;
+use Illuminate\Console\Command;
+
+class CacheUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cache users';
+    /**
+     * @var UserServiceInterface
+     */
+    private UserServiceInterface $userService;
+    /**
+     * @var JsonPlaceHolderClient
+     */
+    private JsonPlaceHolderClient $client;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param UserServiceInterface  $userService
+     * @param JsonPlaceHolderClient $client
+     */
+    public function __construct(UserServiceInterface $userService, JsonPlaceHolderClient $client)
+    {
+        parent::__construct();
+        $this->userService = $userService;
+        $this->client = $client;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->userService->storeUsers($this->client->getUsers());
+        $this->info('Users data cached');
+    }
+}
