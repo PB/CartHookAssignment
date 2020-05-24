89749af76a09cee858f52bcb01f9d9a754dcaf70
PB <b.piotr@gmail.com>
2020-05-24 22:37:58 +0200

    Added Users table migration

--- 2/application/database/migrations/2020_05_24_135012_create_users_table.php
+++ 2/application/database/migrations/2020_05_24_135012_create_users_table.php
@@ -10,17 +10,19 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
+     *
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->jsonb('address');
+            $table->string('phone');
+            $table->string('website');
+            $table->jsonb('company');
         });
     }
 
